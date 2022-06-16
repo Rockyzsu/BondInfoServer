7cd59e765e3afebac34c47550d6dff1b87dd251c
Rockyzsu <jinweizsu@gmail.com>
2022-06-16 23:11:21 +0800

    update type

--- model/bondinfo.go
+++ model/bondinfo.go
@@ -1,16 +1,16 @@
 package model
 
 type Bondinfo struct {
-	Code             string `json:"code"`
-	Name             string `json:"name"`
-	Price            string `json:"price"`
-	PremiumRT        string `json:"premium_rt"`   // 溢价率
-	RedeemIcon       string `json:"redeem_icon"`  // 强赎标志
-	CurrIssAmt       string `json:"curr_iss_amt"` // 剩余规模
-	ZgPrice          string `json:"zg_price"`
-	ConvertPrice     string `json:"convert_price"`
-	Deadline         string `json:"dead_line"`
-	Volume           string `json:"volume"`
-	Rating           string `json:"rating"`
-	ConvertStartDate string `json:"convert_start_date"`
+	Code             string  `json:"code"`
+	Name             string  `json:"name"`
+	Price            float32 `json:"price"`
+	PremiumRT        float32 `json:"premium_rt"`   // 溢价率
+	RedeemIcon       string  `json:"redeem_icon"`  // 强赎标志
+	CurrIssAmt       float32 `json:"curr_iss_amt"` // 剩余规模
+	ZgPrice          float32 `json:"zg_price"`
+	ConvertPrice     float32 `json:"convert_price"`
+	Deadline         string  `json:"dead_line"`
+	Volume           float32 `json:"volume"`
+	Rating           string  `json:"rating"`
+	ConvertStartDate string  `json:"convert_start_date"`
 }
